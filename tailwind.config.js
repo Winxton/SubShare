e0b1311e5d0a9d57b6d5e65e1f05607844081f93
Winston Wu <winxton@gmail.com>
2023-10-16 15:57:36 -0400

    Add tailwind installation.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // Add this line to enable PurgeCSS
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
