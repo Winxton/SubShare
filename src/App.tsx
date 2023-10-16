ff626a940ab696f603051780e21a0c40c6a43f6e
Winston Wu <winxton@gmail.com>
2023-10-16 16:58:07 -0400

    Add all images. Display list of images.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { Button } from '@chakra-ui/react'
+import { Button,Text, Box, Flex, Square } from '@chakra-ui/react'
+import { SimpleGrid } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+
+import netflixImage from './images/netflix.png';
+import hboImage from './images/hbo.png';
 
 function App() {
+  const subscriptions = [
+    {
+      name: 'Netflix',
+      image: netflixImage
+    },
+    {
+      name: 'HBO',
+      image: hboImage
+    }
+  ]
+
   return (
     <div className="App">
-      <div className="bg-green-500">
-          This div has a blue background.
-        </div>
 
-        <Button variant="outline">Button</Button>
+      <Box>
+        <Heading size='md'>Subscriptions</Heading>
+        <Flex >
+          {subscriptions.map((subscription) => (
+            <Square margin="5px" size='150px' border="1px solid grey">
+              <Image src={subscription.image} alt="netflix" />
+            </Square>
+          ))}
+        </Flex>
+      </Box>
     </div>
   );
 }
