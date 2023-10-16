a4b93fe2da90a86d284845c97e3ac5e34db7ab1d
Winston Wu <winxton@gmail.com>
2023-10-16 18:06:18 -0400

    Wrap the text.

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { Button,Text, Box, Flex, Square, Input } from '@chakra-ui/react'
-import { SimpleGrid } from '@chakra-ui/react'
-import { Heading } from '@chakra-ui/react'
-import { Image } from '@chakra-ui/react'
+import { Button, Text, Box, Flex, Square, Input, Wrap, WrapItem } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react';
 
 import netflixImage from './images/netflix.png';
 import hboImage from './images/hbo.png';
 
-import disney from './images/disney.png'
-import spotify from './images/spotify.png'
+import disney from './images/disney.png';
+import spotify from './images/spotify.png';
 
 import youtubeImage from './images/youtube.png';
 import crunchyrollImage from './images/crunchyroll.png';
-
 
 function App() {
   const [searchText, setSearchText] = React.useState<string>('');
@@ -23,51 +21,54 @@ function App() {
   const subscriptions = [
     {
       name: 'Netflix',
-      image: netflixImage
+      image: netflixImage,
     },
     {
       name: 'HBO',
-      image: hboImage
+      image: hboImage,
     },
     {
       name: 'Disney',
-      image: disney
+      image: disney,
     },
     {
       name: 'Spotify',
-      image: spotify
+      image: spotify,
     },
-    { name: 'Youtube',
-      image: youtubeImage
+    {
+      name: 'Youtube',
+      image: youtubeImage,
     },
     {
       name: 'Crunchy',
-      image: crunchyrollImage
+      image: crunchyrollImage,
     },
-  ]
+  ];
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setSearchText(event.target.value)
+    setSearchText(event.target.value);
   }
 
   const filteredSubscriptions = subscriptions.filter((subscription) => {
-    return subscription.name.toLowerCase().includes(searchText.toLowerCase())
-  })
+    return subscription.name.toLowerCase().includes(searchText.toLowerCase());
+  });
 
   return (
     <div className="App">
       <Box>
-        <Heading size='lg'>Create Group</Heading>
-        <Heading size='md'>Subscriptions</Heading>
+        <Heading size="lg">Create Group</Heading>
+        <Heading size="md">Subscriptions</Heading>
 
-        <Input placeholder='Search Subscriptions' size='sm' value={searchText} onChange={handleInputChange} />
-        <Flex >
+        <Input placeholder="Search Subscriptions" size="sm" value={searchText} onChange={handleInputChange} />
+        <Wrap>
           {filteredSubscriptions.map((subscription) => (
-            <Square margin="5px" size='150px' border="1px solid grey">
-              <Image src={subscription.image} alt="netflix" />
-            </Square>
+            <WrapItem key={subscription.name} margin="5px">
+              <Square size="150px" border="1px solid grey">
+                <Image src={subscription.image} alt={subscription.name} />
+              </Square>
+            </WrapItem>
           ))}
-        </Flex>
+        </Wrap>
       </Box>
     </div>
   );
