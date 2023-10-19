68fc49b78acb5e86076b68feb7969cb31aaf6a03
Tommy Yang <tommy_yang621@hotmail.com>
2023-10-18 22:38:07 -0400

    added a color theme.js and started on hardcoded UI

--- src/GroupList.tsx
+++ src/GroupList.tsx
@@ -1,6 +1,45 @@
-
-import { Flex } from '@chakra-ui/react';
+import {
+  Container,
+  Box,
+  Flex,
+  useTheme,
+  Avatar,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 
 export default function GroupList() {
-    return <Flex>This show show a list of groups.</Flex>
+  const theme = useTheme();
+  const subscriptionCost = "$8.10";
+  const savings = "$14.90";
+  const profilePicture = "https://bit.ly/sage-adebayo";
+  return (
+    <Container bg={theme.colors.secondary[600]}>
+      <Box
+        className="Header"
+        bg={theme.colors.primary[500]}
+        w="100%"
+        p={4}
+        color="white"
+      >
+        <Heading as="h1" size="1xl" textAlign="center">
+          Groups
+        </Heading>
+      </Box>
+      <Flex className="profile" margin="10px">
+        <Avatar src={profilePicture} marginRight="10px" />
+        <Box>
+          <Text fontWeight="bold"> Total Subscriptions</Text>
+          <Text>
+            You owe{" "}
+            <Text as="span" fontWeight="bold">
+              {subscriptionCost}
+            </Text>
+            /month
+          </Text>
+          <Text> {savings} saved</Text>
+        </Box>
+      </Flex>
+    </Container>
+  );
 }
---
+++ src/theme.tsx
@@ -0,0 +1,15 @@
+// theme.ts
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    primary: {
+      500: "#ff5349", // Primary color
+    },
+    secondary: {
+      600: "#D3D3D3", //secondary color
+    },
+  },
+});
+
+export default theme;
