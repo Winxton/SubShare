56706bb1b098d1f9038b6d5e675caa16a6034077
Tommy Yang <tommy_yang621@hotmail.com>
2023-10-23 14:24:33 -0400

    skeleton ui for new group

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -1,12 +1,20 @@
+import { Flex, Text, Square } from "@chakra-ui/react";
+import { CheckCircleIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
-import { Flex, Text } from '@chakra-ui/react';
+export function Friend(props: { name: string }) {
+  const [showCheckCircle, setShowCheckCircle] = useState(false);
 
-export function Friend(props: {
-    name: string,
-}) {
-    return (
-        <Flex>
-            <Text>{props.name}</Text>
-        </Flex>
-    )
+  const selectName = () => {
+    setShowCheckCircle(!showCheckCircle);
+  };
+
+  return (
+    <Flex>
+      <Square size="150px" border="1px solid grey" onClick={selectName}>
+        <Text>{props.name}</Text>
+      </Square>
+      {showCheckCircle && <CheckCircleIcon />}
+    </Flex>
+  );
 }
