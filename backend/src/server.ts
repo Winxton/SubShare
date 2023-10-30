2b1ae141700ea1db772b4aa9560ceffdfbbdcb66
Winston Wu <winxton@gmail.com>
2023-10-30 14:44:35 -0400

    Add API for groups.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,47 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 4000;
 
-// Sample route
-app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello, World!' });
+app.use(bodyParser.json());
+
+class Subscription {
+    constructor(public name: string, public image: string, public cost: number) {}
+}
+
+class Group {
+    constructor(public subscription: Subscription, public friends: Friend[]) {}
+}
+
+class Friend {
+    constructor (public name: string, public image: string) {}
+}
+
+let groups: Group[] = [];  // This will serve as our in-memory database for now
+
+// List all groups
+app.get('/api/groups', (req, res) => {
+    res.json(groups);
+});
+
+// Create a new group
+app.post('/api/groups', (req, res) => {
+    const { subscription, friends } = req.body;
+    const newGroup = new Group(subscription, friends);
+    groups.push(newGroup);
+    res.status(201).json(newGroup);
+});
+
+// Delete a group
+app.delete('/api/groups/:index', (req, res) => {
+    const index = parseInt(req.params.index, 10);
+    if (index >= 0 && index < groups.length) {
+        const deletedGroup = groups.splice(index, 1);
+        res.json(deletedGroup);
+    } else {
+        res.status(404).send('Group not found');
+    }
 });
 
 app.listen(PORT, () => {
