1c36e5dd6ff183ebf2c21933fe198b065547d9d1
Tommy Yang <tommy_yang621@hotmail.com>
2023-11-03 18:44:48 -0400

    made the view group dynamic

--- frontend/src/components/GroupList.tsx
+++ frontend/src/components/GroupList.tsx
@@ -9,7 +9,7 @@ import {
   Stack,
   StackDivider,
   Button,
-  useDisclosure
+  useDisclosure,
 } from "@chakra-ui/react";
 
 import { Subscription as SubscriptionComponent } from "./Subscription";
@@ -17,15 +17,14 @@ import { Subscription } from "../models/Subscription";
 import netflixImage from "../images/netflix.png";
 import spotify from "../images/spotify.png";
 import disney from "../images/disney.png";
-import NewGroup from './NewGroup'
-import React,{ useEffect, useState } from "react";
+import NewGroup from "./NewGroup";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Group } from "../models/Group";
 
-
 export default function GroupList() {
   const theme = useTheme();
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [groups, setGroups] = useState<Group[]>([]);
   const [loading, setLoading] = useState(true);
   const subscriptionCost = "$8.10";
@@ -34,21 +33,27 @@ export default function GroupList() {
 
   useEffect(() => {
     fetch("http://localhost:4000/api/groups")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      setGroups(data.map((groupData: any) => {
-        return new Group(
-          new Subscription(groupData.subscription.name, groupData.subscription.image, groupData.subscription.cost),
-          groupData.members
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setGroups(
+          data.map((groupData: any) => {
+            return new Group(
+              new Subscription(
+                groupData.subscription.name,
+                groupData.subscription.image,
+                groupData.subscription.cost
+              ),
+              groupData.members
+            );
+          })
         );
-      }));
-      setLoading(false);
-    })
+        setLoading(false);
+      });
   }, [isOpen]);
 
   if (loading) {
@@ -64,15 +69,11 @@ export default function GroupList() {
     },
   ];
 
-  console.log("GROUPS")
-  console.log(groups)
+  // console.log("GROUPS");
+  // console.log(groups);
   return (
-    <Container maxW='3xl'>
-      <Box
-        className="Header"
-        w="100%"
-        p={4}
-      >
+    <Container maxW="3xl">
+      <Box className="Header" w="100%" p={4}>
         <Heading as="h1" size="md" textAlign="left">
           Groups
         </Heading>
@@ -104,20 +105,25 @@ export default function GroupList() {
         divider={<StackDivider borderColor="gray.200" />}
         spacing={4}
         align="stretch"
-        borderRadius={'md'}
+        borderRadius={"md"}
         padding="2"
       >
-      <Flex align="center" justify="space-between">
-        <Box>
-          <Text fontWeight="bold">My Groups</Text>
-        </Box>
-        <Box>
-          <Button colorScheme="blue" onClick={onOpen}>New Group</Button>
-        </Box>
-      </Flex>
-      
-      {groups.map((group) => (
-          <Link to={`/view-group/${group.subscription.name}`} key={group.subscription.name}>
+        <Flex align="center" justify="space-between">
+          <Box>
+            <Text fontWeight="bold">My Groups</Text>
+          </Box>
+          <Box>
+            <Button colorScheme="blue" onClick={onOpen}>
+              New Group
+            </Button>
+          </Box>
+        </Flex>
+
+        {groups.map((group) => (
+          <Link
+            to={`/view-group/${group.subscription.name}`}
+            key={group.subscription.name}
+          >
             <SubscriptionComponent
               image={group?.subscription?.image}
               cost={group?.subscription?.cost.toString()}
@@ -139,9 +145,7 @@ export default function GroupList() {
         ))}
       </Stack>
 
-
-      {isOpen && <NewGroup onClose={onClose}/>}
-
+      {isOpen && <NewGroup onClose={onClose} />}
     </Container>
   );
 }
--- frontend/src/components/ViewGroup.tsx
+++ frontend/src/components/ViewGroup.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {
   Container,
   Heading,
   Box,
-  HStack,
   Image,
   VStack,
   Text,
@@ -13,30 +12,57 @@ import {
   Avatar,
   Center,
 } from "@chakra-ui/react";
-import spotify from '../images/spotify.png';
-import friend1 from '../images/friend1.jpg';
-import friend2 from '../images/friend2.jpg';
-import friend3 from '../images/friend3.jpg';
-import friend4 from '../images/friend4.jpg';
+import spotify from "../images/spotify.png";
+import friend1 from "../images/friend1.jpg";
+import friend2 from "../images/friend2.jpg";
+import friend3 from "../images/friend3.jpg";
+import friend4 from "../images/friend4.jpg";
 import { Friend } from "./Friend";
+import { Subscription } from "../models/Subscription";
+import { Group } from "../models/Group";
 
 export default function ViewGroup() {
   const theme = useTheme();
   const { groupName } = useParams();
-
+  const [selectGroup, setSelectGroup] = useState<Group | null>(null);
+  const savedAmount = "10";
+  const owedAmount = "5";
   // Use the `groupName` as part of the group name
-  const group = {
-    name: `${groupName}'s Spotify Group`,
-    owedAmount: "$3.75",
-    savedAmount: "$0.75",
-    members: [
-      { name: "Albert Snow", image: friend1, isOwner: true, isMe: false },
-      { name: "Chrisa Jenkins", image: friend2, isMe: false },
-      { name: "Bobby Miller", image: friend3, isMe: false },
-      { name: "Christine", image: friend4, isMe: true },
-    ],
-  };
 
+  useEffect(() => {
+    console.log(groupName);
+    fetch(`http://localhost:4000/api/view-group/?groupName=${groupName}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((groupData) => {
+        console.log(`filteredGroup ${groupData}`);
+        if (groupData.length > 0) {
+          // Assuming the server returns an array, but you can modify the structure as needed
+          const firstGroup = groupData[0];
+          setSelectGroup(
+            new Group(
+              new Subscription(
+                firstGroup.subscription.name,
+                firstGroup.subscription.image,
+                firstGroup.subscription.cost
+              ),
+              firstGroup.friends
+            )
+          );
+        } else {
+          // Handle the case where no matching group is found
+          setSelectGroup(null);
+        }
+      });
+  }, [groupName]);
+
+  if (selectGroup === null) {
+    return <div>No matching group found.</div>;
+  }
   return (
     <Container bg={theme.colors.secondary[600]}>
       <Box
@@ -47,7 +73,7 @@ export default function ViewGroup() {
         color="white"
       >
         <Heading as="h1" size="1xl" textAlign="center">
-          {group.name}
+          {selectGroup.subscription.name} Group
         </Heading>
       </Box>
 
@@ -56,24 +82,23 @@ export default function ViewGroup() {
         <Box bg="gray.200" p={4} borderRadius="md">
           <Center mb={4}>
             <Image
-              src={spotify}
+              src={selectGroup.subscription.image}
               boxSize="100px"
               objectFit="contain"
               alt="Spotify Logo"
             />
           </Center>
 
-    <Text mt={2} textAlign="center">
-      You owe {group.members.find((member) => member.isMe)?.name || "me"}{" "}
-      <Text as="span" fontWeight="bold">
-        {group.owedAmount}
-      </Text>
-    </Text>
-
+          <Text mt={2} textAlign="center">
+            {/* You owe {selectGroup.members.find((member) => member.isMe)?.name || "me"}{" "} */}
+            <Text as="span" fontWeight="bold">
+              {owedAmount}
+            </Text>
+          </Text>
 
           <Text textAlign="center">
             <Text as="span" fontWeight="bold" color="green">
-              {group.savedAmount}{" "}
+              {savedAmount}{" "}
             </Text>
             saved
           </Text>
@@ -84,8 +109,8 @@ export default function ViewGroup() {
             Members
           </Heading>
 
-          {group.members.map((member) => (
-            <Friend name={member.name} image={member.image} isMe={member.isMe} />
+          {selectGroup.friends.map((member) => (
+            <Friend name={member.name} image={member.image} />
           ))}
         </VStack>
       </Box>
