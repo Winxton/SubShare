1d2e0f51fa1b834185f2984504cb8b0d9b9c90e4
deviationtv <youngsukgfb@gmail.com>
2023-11-06 15:32:33 -0500

    Removed private key

--- backend/example.ts
+++ backend/example.ts
@@ -1,8 +1,8 @@
 
 const { createClient } = require('@supabase/supabase-js')
 
-const SUPABASE_URL = 'https://vevmecjipzmcuntimfrh.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZldm1lY2ppcHptY3VudGltZnJoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5ODA5NzcyNCwiZXhwIjoyMDEzNjczNzI0fQ.jtLcWLQjThi0M78kOspkfZQ2tjVRYBgwkeK-zNctOm0'
+const SUPABASE_URL = ''
+const SUPABASE_ANON_KEY = ''
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
