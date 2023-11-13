3856beb2f7563e796695148dc82d943c7d939dc5
Winston Wu <winxton@gmail.com>
2023-11-13 16:13:05 -0500

    Comments.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -36,6 +36,8 @@ let friends: Friend[] = [
   new Friend("young", "https://bit.ly/sage-adebayo"),
 ];
 
+// API routes related to friends
+
 app.get("/api/friends", (req, res) => {
   res.json(friends);
 });
@@ -73,6 +75,8 @@ app.delete("/api/friends/:name", (req, res) => {
   const deletedFriend = friends.splice(index, 1);
   res.json({ message: "Friend deleted", friend: deletedFriend[0] });
 });
+
+// API routes related to groups
 
 //get selected groups
 app.get("/api/groups", (req, res) => {
