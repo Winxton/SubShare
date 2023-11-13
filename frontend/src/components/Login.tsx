5054ec2bd0dc0238a8035749eb28c14c81ca35c9
ahmedmomomed <108437884+ahmedmomomed@users.noreply.github.com>
2023-11-13 15:38:47 -0500

    loging page

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,18 +1,24 @@
+import { Button, Container, Input, Image } from "@chakra-ui/react";
 
-import { Button, Container, Input } from '@chakra-ui/react'
-
+// Your image URL
+const loginImageURL =
+  "https://media.discordapp.net/attachments/1163688273521152062/1173708850256433152/DALLE_Cartoon_Subscription.png?ex=6564f08c&is=65527b8c&hm=7c2c67d1e7c888feb1d09c6d384629d55f0d98d27a111854043fba698ea23827&=&width=627&height=627";
 
 export default function Login() {
+  function loginWithOTP() {
+    // TODO: Implement OTP functionality
+  }
 
-    function loginWithOTP() {
-        // TODO(tommy): This should send an OTP to the user's email, and then redirect to the main page.
-    }    
+  return (
+    <Container maxW="3xl" centerContent>
+      {/* Add your image using the Image component */}
+      <Image src={loginImageURL} alt="Login Image" boxSize="300px" mb={4} />
 
-    // TODO:(nina) Add the logo here
-    return (
-        <Container maxW="3xl">
-            <Input placeholder="Email" />
-            <Button colorScheme="blue">Login</Button>
-        </Container>
-    )
+      <Input placeholder="Email" w="300px" mb={4} />
+
+      <Button colorScheme="blue" onClick={loginWithOTP}>
+        Login
+      </Button>
+    </Container>
+  );
 }
