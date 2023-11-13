e526d4dbadd45ec6d28f10edf1571aa2e60bb319
Tommy Yang <tommy_yang621@hotmail.com>
2023-11-13 16:47:15 -0500

    otp

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,18 +1,40 @@
-
-import { Button, Container, Input } from '@chakra-ui/react'
-
+import { Button, Container, Input } from "@chakra-ui/react";
+import { useState } from "react";
+import { supabase } from "./Main";
+import { API_URL } from "../constants";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const loginWithOTP = async (event) => {
+    const { data, error } = await supabase.auth.signInWithOtp({
+      email: email,
+      options: {
+        // set this to false if you do not want the user to be automatically signed up
+        shouldCreateUser: true,
+        emailRedirectTo: API_URL,
+      },
+    });
+    console.log(data);
+    console.log(error);
 
-    function loginWithOTP() {
-        // TODO(tommy): This should send an OTP to the user's email, and then redirect to the main page.
-    }    
+    if (error) {
+      alert(error.message);
+    } else {
+      alert("Check your email for the login link!");
+    }
+  };
 
-    // TODO:(nina) Add the logo here
-    return (
-        <Container maxW="3xl">
-            <Input placeholder="Email" />
-            <Button colorScheme="blue">Login</Button>
-        </Container>
-    )
+  // TODO:(nina) Add the logo here
+  return (
+    <Container maxW="3xl">
+      <Input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Button colorScheme="blue" onClick={loginWithOTP}>
+        Login
+      </Button>
+    </Container>
+  );
 }
