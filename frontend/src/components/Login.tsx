324f9b563cfa5321561f9aa9138ec0e44d6b4996
ahmedmomomed <108437884+ahmedmomomed@users.noreply.github.com>
2023-11-13 16:08:39 -0500

    revised login image

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,8 +1,6 @@
 import { Button, Container, Input, Image } from "@chakra-ui/react";
-
+import loginImageURL from "../images/SubscriptionAppLogo.png";
 // Your image URL
-const loginImageURL =
-  "https://media.discordapp.net/attachments/1163688273521152062/1173708850256433152/DALLE_Cartoon_Subscription.png?ex=6564f08c&is=65527b8c&hm=7c2c67d1e7c888feb1d09c6d384629d55f0d98d27a111854043fba698ea23827&=&width=627&height=627";
 
 export default function Login() {
   function loginWithOTP() {
