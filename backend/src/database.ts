15dad3502c712effdbfcb558de8e6e89352604c0
Winston Wu <winxton@gmail.com>
2023-11-13 21:40:59 -0500

    cleanup

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -2,42 +2,11 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const { createClient } = require("@supabase/supabase-js");
-const APP_URL = "http://localhost:3000";
-
-interface User {
-  email: string;
-}
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SECRET_KEY
 );
-
-async function signInWithEmail(email: string) {
-  const { data, error } = await supabase.auth.signInWithOtp({
-    email: email,
-    options: {
-      // set this to false if you do not want the user to be automatically signed up
-      shouldCreateUser: true,
-      emailRedirectTo: APP_URL,
-    },
-  });
-  console.log(data);
-  console.log(error);
-}
-
-// Inserting a user
-async function insertUser(supabase, email) {
-  const { data, error } = await supabase.from("users").insert([{ email }]);
-}
-
-// Selecting all users
-async function selectUsers(supabase) {
-  const { data, error } = await supabase.from("users").select("*");
-
-  const users = data as User[];
-  users.map((user) => console.log(user.email));
-}
 
 // Get User by Token
 export async function getUser(accessToken: string) {
