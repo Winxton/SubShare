9ba073e1e28a9d4e5b791b29508fd160105b4231
Winston Wu <winxton@gmail.com>
2024-01-15 16:28:16 -0500

    Fix env path.

--- backend/src/repository/database.ts
+++ backend/src/repository/database.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 const path = require("path");
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 
 import { Friend, Group, Subscription } from "../models/models";
 const { createClient } = require("@supabase/supabase-js");
